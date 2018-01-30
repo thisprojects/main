597f2e783057e6c70a0ac38a40065140e936a5f7
Nathan Downes <nathandownes@Andys-iMac.csv.warwick.ac.uk>
2018-01-30 16:58:53 +0000

    experiments with facebook api

--- facebook/src/App.js
+++ facebook/src/App.js
@@ -4,40 +4,67 @@ import './App.css';
 
 class App extends Component {
     
+componentDidMount() {  // facebook signin  button render
+    
+    window.fbAsyncInit = function() {
+            console.log("test"); 
+            window.FB.init({
+            appId      : '527899994260788',
+            cookie     : true,
+            xfbml      : true,
+            version    : 'v2.11'
+            });
+      
+           window.FB.AppEvents.logPageView();  
+       window.FB.getLoginStatus(function(response) {
+              if (response.status === 'connected') {
+                console.log('Logged in.');
+  }
+  else {
+    window.FB.login();
+  }
+});
+   window.FB.getLoginStatus(function(response) {
+  
+    const user = response.authResponse.userID; 
+    console.log(user);
+       window.FB.api(user, {fields:'first_name,last_name,picture'}, function(response) {
+      console.log(response); 
+  }
+);
+});     
+        
+      };
 
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "https://connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
+    
+ 
+    
+}
     
   render() {
       
-      var egg = <h1> Snuggems </h1>;
-      
-      function sweggs () {
-          
-          return (
-          
-          <div>
-              hello
-          </div>      
-          )
-          
-      };
+             
       
     return (
-      
-
-
-      
+       
       <div className="App">
-        {egg}
-        {sweggs()}
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-      
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+
+        <h1 className="App-title">Social App</h1>
+        
+     
+        
+        <div className="login">
+        
+        <button>Login With Facebook </button>
+        </div>
+        </div>
     );
 
 
