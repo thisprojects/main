d5f098acbb0a175025c7190dbb161d16100fe298
Nathan Downes <nathandownes@Andys-iMac.csv.warwick.ac.uk>
2018-02-08 17:24:01 +0000

    adding updated post.php

--- facebook/htdocs/post.php
+++ facebook/htdocs/post.php
@@ -2,31 +2,90 @@
 
   $postdata = file_get_contents("php://input");
   $request = json_decode($postdata);
-  $email = $request->message;
+  $firstname = $request->firstname;
+  $lastname = $request->lastname;
+  $id = $request->id;
 
-  $encode = json_encode($email);
+  $error = "Connection failed: ";
+  $success = "Connected succesfully";
 
-//  echo json_encode("hello");
-  print_r($encode); 
-  $myfile = fopen("newfile.txt", "w") or die("Unable to open file!");
+$lastname = addslashes($lastname);
+$servername = "localhost";
+$username = "test";
+$password = "testing";
+$dbname = "myDB";
 
-  fwrite($myfile, $email);
-  fwrite($myfile, $postdata);
+//if ($firstname == " "){
+
+//  die
+//}
+class JASON {
+      public $firstname = "";
+      public $lastname  = "";
+      public $constatus = "";
+      public $exists = "";
+      public $eventlist = [];
+   }
+
+   $e = new JASON();
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    $return = [connection_status => " connection failed "];
+} else {
+//  $return = [connection_status => " connection succesful "];
+$e->constatus = "connection succesful";
 
 
-  fclose($myfile);
+$sql = "INSERT INTO users (firstname, lastname, idnumber)
+VALUES ('$firstname', '$lastname', '$id')";
+$checkuser ="SELECT * FROM users WHERE firstname = '$firstname' AND lastname = '$lastname' AND idnumber = '$id'";
+$event = "SELECT * FROM events WHERE owner = '$id'";
+$searchresult = $conn->query($checkuser);
+$eventsearch = $conn->query($event);
+
+if ($searchresult->num_rows > 0){
+
+ //$return = [firstname => $firstname , lastname => $request->lastname , exists => "already exists"];
+ $e->firstname = $firstname;
+ $e->lastname = $request->lastname;
+ $e->exists = "already exists";
 
 
 
-//  $data               = file_get_contents("php://input");
-//  $message            = $dataJsonDecode->message;
-//  $myJSON = json_encode($message);
-//  print_r ($myJSON);
 
-//$json = json_decode(file_get_contents('php://input'), true);
-//$output = json_encode($json);
+          if ($eventsearch->num_rows > 0) {
 
-//$egg = json_encode("hello");
-//print_r($egg);
+            while($row = $eventsearch->fetch_assoc()) {
+            //  array_push($e->eventlist, $row["ID"]);
+            $e->eventlist[Events] [] = $row;
+    // echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
+ }
+
+
+          }else{
+
+            // $return->events = "No events";
+            $e->eventslist = "no events";
+          }
+
+}else {
+
+if ($conn->query($sql) === TRUE) {
+    $return = [record_create => " New record created successfully"];
+} else {
+    $return .= " Error: " . $sql . " " . $conn->error;
+}
+}
+}
+$encode = json_encode($e);
+
+
+  print_r($encode);
+
+
+
 
 ?>
