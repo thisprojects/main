9c35d655d429b5b4130232b06cfe3bfe420fccd6
Nathan@downes.com <Nathan@downes.com>
2018-04-12 21:56:08 +0100

    added quote bank and logic to prevent repeat entries

--- quote/src/quote.jsx
+++ quote/src/quote.jsx
@@ -1,6 +1,57 @@
 import React from 'react';
 import Fetch from 'react-fetch';
 
+class SaveQuote extends React.Component {
+
+render() {
+
+
+
+if (this.props.saved.length){
+
+  return (
+<div>
+{this.props.exists ? <p>This Quote Is Already In The Bank!</p> : <p></p>}
+
+{this.props.saved.map (input  => // map the array and return the output items in HTML list
+
+    <ul key={input.toString()}>
+        <li key={input.toString()}>{input}</li>
+    </ul>
+
+)}
+
+</div>
+)
+
+
+}else {
+
+  return (
+
+    <div></div>
+  )
+}
+
+}
+}
+
+class InputQuote extends React.Component {
+
+render() {
+
+return (
+
+<div>
+Enter your own quote!
+</div>
+
+)
+
+}
+
+}
+
 class DisplayQuote extends React.Component{
 
 render (){
@@ -8,7 +59,8 @@ render (){
 
       let quote = this.props.results
       return (
-          <div>
+
+          <div className="quoteBox">
                   {quote.quoteText}
                   <br/>
                   {quote.quoteAuthor}
@@ -19,7 +71,7 @@ render (){
 
       return (
 
-          <div>
+          <div className="messageBox">
           <h1>{this.props.message}</h1>
           </div>
       )
@@ -33,7 +85,9 @@ class Quote extends React.Component {
 
 constructor (props){
             super(props);
-            this.state = {results: [], waiting: false, tries: 0, message: "" };
+            this.state = {results: ["Re-Rendering - Quote Is Empty"], waiting: false, tries: 0, message: "", savedQuotes: [], exists: false };
+
+
 
         }
 
@@ -57,36 +111,71 @@ console.log(this.state.results);
         <DisplayQuote results={this.state.results} message={this.state.message} />
       </div>
       <div className="buttons">
-      {this.state.waiting ? <button>Please Wait!</button> : <button onClick={this._napTime.bind(this, 1)}>New quote!</button>}
+      {this.state.waiting ? <p>Please Wait!</p> : this._buttons()}
+
+
+
       </div>
+      <InputQuote />
+      <SaveQuote saved={this.state.savedQuotes} exists={this.state.exists}/>
       </div>
 
     );
   }
 
-_napTime(tries){
-this.setState({results:[], waiting: true, message: "Acquiring Inspiration..."});
-setTimeout(this._fetchIT.bind(this, tries), 1500);
+_napTime(apiTries){
+
+
+this.setState({results:["Re-Rendering - Quote Is Empty"], waiting: true, message: "Acquiring Inspiration...", exists: false});
+setTimeout(this._fetchIT.bind(this, apiTries), 1500);
+
+}
+
+_buttons() {
+
+  return (
+    <div>
+    <button id="newQuote" onClick={this._napTime.bind(this, 1)}>New Quote!</button>
+    <button id="saveQuote" onClick={this._saveIt.bind(this)}>Save Quote</button>
+    </div>
+  );
+
+
+}
+
+_saveIt(){
+
+let x = this.state.savedQuotes.indexOf(this.state.results.quoteText);
+console.log (x);
+if (x == -1){
+this.setState({savedQuotes: this.state.savedQuotes.concat([this.state.results.quoteText])});
+
+}else{
+
+this.setState({exists: true})
+console.log ("this quote already exists");
 
 }
 
 
-_fetchIT (tries) {
-
-    fetch('http://localhost:80/quoteproxy.php')
-      .then (response => response.json())
-      .then(data => this.setState({ results: data, waiting:false})).catch((error) => {
-
-        console.log(error + " Bad Json Response - Re-Initiating Call To API , Attempts = " + tries);
-        if (tries >= 5){
-
-          this.setState({message: "API Connection Failed. Please Try Again!", waiting: false});
-          return;
-        }
-
-        this._napTime(tries+=1);
-      });
 }
+
+
+      _fetchIT (apiTries) {
+
+          fetch('http://localhost:80/quoteproxy.php')
+                .then (response => response.json())
+                  .then(data => this.setState({ results: data, waiting:false}))
+                    .catch((error) => {
+                                          console.log(error + " Bad Json Response - Re-Initiating Call To API , Attempts = " + apiTries);
+
+                                            if (apiTries >= 5){
+                                                    this.setState({message: "API Connection Failed. Please Try Again!", waiting: false});
+                                            }else{
+                                                    this._napTime(apiTries+=1);
+                                            }
+         });
+      }
 }
 
 export default Quote;
