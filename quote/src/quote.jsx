6284df4550392d77973b724216a9627e327b5390
Nathan <contact@nathandownes.co.uk>
2018-04-12 13:14:39 +0100

    Catching errors in fetch and refactoring code into components

--- quote/src/quote.jsx
+++ quote/src/quote.jsx
@@ -1,17 +1,45 @@
 import React from 'react';
 import Fetch from 'react-fetch';
 
+class DisplayQuote extends React.Component{
+
+render (){
+  if (this.props.results.quoteText){
+
+      let quote = this.props.results
+      return (
+          <div>
+                  {quote.quoteText}
+                  <br/>
+                  {quote.quoteAuthor}
+          </div>
+      )
+
+  }else{
+
+      return (
+
+          <div>
+          <h1>{this.props.message}</h1>
+          </div>
+      )
+  }
+
+
+
+}
+}
 class Quote extends React.Component {
 
 constructor (props){
             super(props);
-            this.state = {results: [], waiting: false };
+            this.state = {results: [], waiting: false, tries: 0, message: "" };
 
         }
 
 componentDidMount () {
 
- this._napTime();
+ this._napTime(1);
 
 }
 
@@ -26,55 +54,38 @@ console.log(this.state.results);
       </div>
 
       <div className="quote">
-        {this._jsonList()}
+        <DisplayQuote results={this.state.results} message={this.state.message} />
       </div>
-      {this.state.waiting ? <button>Please Wait!</button> : <button onClick={this._napTime.bind(this)}>New quote!</button>}
+      <div className="buttons">
+      {this.state.waiting ? <button>Please Wait!</button> : <button onClick={this._napTime.bind(this, 1)}>New quote!</button>}
+      </div>
       </div>
 
     );
   }
 
-_napTime(){
-this.setState({results:[], waiting: true});
-setTimeout(this._fetchIT.bind(this), 1500);
+_napTime(tries){
+this.setState({results:[], waiting: true, message: "Acquiring Inspiration..."});
+setTimeout(this._fetchIT.bind(this, tries), 1500);
 
 }
-_jsonList (){
 
-    if (this.state.results.quoteText){
 
-        let quote = this.state.results
-        return (
-            <div>
-                    {quote.quoteText}
-                    <br/>
-                    {quote.quoteAuthor}
-            </div>
-        )
-
-    }else{
-
-        return (
-
-            <div>
-            <h1>Gathering Inspiration...</h1>
-            </div>
-        )
-    }
-}
-
-_fetchIT () {
-
+_fetchIT (tries) {
 
     fetch('http://localhost:80/quoteproxy.php')
       .then (response => response.json())
-      .then(data => this.setState({ results: data, waiting:false })).catch((error) => {
-        console.log(error + " Bad Json Response - Re-Initiating Call To API")
+      .then(data => this.setState({ results: data, waiting:false})).catch((error) => {
 
-        this._napTime();
+        console.log(error + " Bad Json Response - Re-Initiating Call To API , Attempts = " + tries);
+        if (tries >= 5){
 
+          this.setState({message: "API Connection Failed. Please Try Again!", waiting: false});
+          return;
+        }
+
+        this._napTime(tries+=1);
       });
-
 }
 }
 
