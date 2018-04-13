3e7e13acfbaff8b5a57741bf5b2975ae93b2dadf
Nathan@downes.com <Nathan@downes.com>
2018-04-13 22:38:13 +0100

    Finished and fixed fade anims

--- quote/src/quote.jsx
+++ quote/src/quote.jsx
@@ -4,155 +4,121 @@ import './App.css';
 
 class SaveQuote extends React.Component {
 
-render() {
+    render() {
 
+        if (this.props.saved.length){
 
+          return (
+                    <div>
 
-if (this.props.saved.length){
+                          {this.props.exists ?
+                                            <p className="exists fadeOut">This Quote And Author Combination Is Already In The Bank!</p>
+                                            : <p></p> }
 
-  return (
-<div>
-<div>
-{this.props.exists ? <p className="fadeOut">This Quote Is Already In The Bank!</p> : <p></p>}
-</div>
-<div>
-{this.props.saved.map (input  => // map the array and return the output items in HTML list
+                          {this.props.saved.map (input  => // map the array and return the output items in HTML list
 
-    <ul key={input.toString()}>
-        <li key={input.toString()}>{input}</li>
-    </ul>
+                              <ul key={input.toString()}>
+                                  <li className="fadeIn" key={input.toString()}>{input}</li>
+                              </ul>
+                          )}
 
-)}
-
-</div>
-</div>
-)
-
-
-}else {
-
-  return (
-
-    <div></div>
-  )
-}
-
-}
-}
-
-
-
-class InputQuote extends React.Component {
-
-render() {
-
-return (
-
-<div>
-Input Your quote Here
-</div>
-
-)
-
-}
-
+                    </div>
+                )
+        } else {
+                return (
+                          <div></div>
+                )
+       }
+    }
 }
 
 class DisplayQuote extends React.Component{
 
-render (){
-  if (this.props.results.quoteText){
+    render (){
 
       let quote = this.props.results
-      return (
 
-          <div className="quoteBox">
-                  {quote.quoteText}
-                  <br/>
-                  {quote.quoteAuthor}
-          </div>
-      )
+      if (quote.quoteText){
 
-  }else{
+          return (
 
-      return (
+            <div className="quoteBox fadeIn">
+                 {quote.quoteText + " - " + quote.quoteAuthor}
+            </div>
+          )
 
-          <div className="messageBox">
-          <h1>{this.props.message}</h1>
-          </div>
-      )
-  }
+      } else {
+              return (
 
-
-
+                  <div className="messageBox">
+                      <h1>{this.props.message}</h1>
+                  </div>
+              )
+      }
+   }
 }
-}
+
 class Quote extends React.Component {
 
 constructor (props){
-            super(props);
-            this.state = {results: ["Re-Rendering - Quote Is Empty"], waiting: false, tries: 0, message: "", savedQuotes: [], exists: false, toggle: true, formValue: "" };
-
-
-
-        }
+                    super(props);
+                    this.state = {results: [], waiting: false, message: "", savedQuotes: [], exists: false, toggle: true, formValue: "" };
+}
 
 componentDidMount () {
 
- this._napTime(1);
-
+    this._napTime(1);
 }
-
   render() {
 
-console.log(this.state.results);
     return (
       <div className="quote">
 
-      <div className="header">
-          <h1>Amazing Quotes</h1>
-      </div>
+            <div className="header">
+                <h1>Amazing Quotes</h1>
+            </div>
 
-      <div className="quote">
-      {this.state.toggle ? <DisplayQuote results={this.state.results} message={this.state.message} /> : this._textBox()}
-      </div>
-      <div className="buttons">
-      {this.state.waiting ? <div></div> : this._buttons()}
-      </div>
+            <div className="quote">
+                  {this.state.toggle ?
+                                    <DisplayQuote results={this.state.results} message={this.state.message} />
+                                     : this._inputBox()}
+            </div>
 
-      <SaveQuote saved={this.state.savedQuotes} exists={this.state.exists} />
-      </div>
+            <div className="buttons">
+                  {this.state.waiting ?
+                                      <div></div>
+                                      : this._buttons()}
+            </div>
 
+            <SaveQuote saved={this.state.savedQuotes} exists={this.state.exists} />
+      </div>
     );
   }
 
-_textBox() {
+_inputBox() {
 
-
-return (
-  <form onSubmit={this._saveIt.bind(this, this.state.formValue, "Yourself")}>
-      <label>
-          <input type="text" value={this.state.formValue} placeholder="Enter Your Quote!" onChange={this._handleChange.bind(this)} />
-      </label>
-      <input type="submit" value="Submit Quote" />
-  </form>
-)
-
-}
-
-_handleChange (event) {
-
- this.setState({formValue: event.target.value, exists: false}); // updates our name as it is typed into the box
+    return (
+      <form onSubmit={this._saveIt.bind(this, this.state.formValue, "Yourself")}>
+          <label>
+              <input type="text" value={this.state.formValue} placeholder="Enter Your Quote!" onChange={this._updateAsYouType.bind(this)} />
+          </label>
+          <input type="submit" value="Save Quote" />
+      </form>
+    )
 
 }
 
+_updateAsYouType (event) {
 
+    this.setState({formValue: event.target.value}); // updates our quote as it is typed into the box
+
+}
 
 _napTime(apiTries){
 
+    this.setState({results:[], waiting: true, message: "Acquiring Inspiration...", toggle: true});
 
-this.setState({results:["Re-Rendering - Quote Is Empty"], waiting: true, message: "Acquiring Inspiration...", exists: false, toggle: true});
-setTimeout(this._fetchIT.bind(this, apiTries), 1500);
+    setTimeout(this._fetchIT.bind(this, apiTries), 1500);
 
 }
 
@@ -161,47 +127,44 @@ _buttons() {
   return (
     <div>
     <button id="newQuote" onClick={this._napTime.bind(this, 1)}>Get Random Quote!</button>
+
     {this.state.toggle?
                       <button id="saveQuote" onClick={this._saveIt.bind(this, this.state.results.quoteText, this.state.results.quoteAuthor)}>Save Quote</button>
                       : <b></b> }
+
     <button className="inputQbutton" onClick={this._toggleQuote.bind(this)}>Enter Your Own Quote!</button>
     </div>
   );
-
 
 }
 
 _toggleQuote() {
 
-this.setState({toggle: false, exists:false, results: []})
-console.log(this.state.toggle);
+      this.setState({toggle: false, results: []}) // toggles between Entering own quote and getting a random one. Also resets any results already acquired from API.
 
 }
 
 _saveIt(quoteText, quoteAuthor, event){
 
-event.preventDefault();
+    event.preventDefault();   //prevents form submit from reloading the page
 
+    let z = quoteText + " - " + quoteAuthor
+    let x = this.state.savedQuotes.indexOf(z);
 
-let z = quoteText + " - " + quoteAuthor
-let x = this.state.savedQuotes.indexOf(z);
+        if (x === -1){
+                        this.setState({savedQuotes: this.state.savedQuotes.concat([z])});
+        }else{
+                        this.setState({exists: true});
 
-if (x === -1){
-this.setState({savedQuotes: this.state.savedQuotes.concat([z])});
-
-}else{
-
-this.setState({exists: true})
-console.log ("this quote already exists");
-
-
+                        setTimeout(this._fadeTimer.bind(this), 3000);
+        }
 }
 
+  _fadeTimer () {
+                   this.setState({exists: false})
+  }
 
-}
-
-
-      _fetchIT (apiTries) {
+  _fetchIT (apiTries) {
 
           fetch('http://localhost:80/quoteproxy.php')
                 .then (response => response.json())
@@ -211,11 +174,11 @@ console.log ("this quote already exists");
 
                                             if (apiTries >= 5){
                                                     this.setState({message: "API Connection Failed. Please Try Again!", waiting: false});
-                                            }else{
+                                            } else {
                                                     this._napTime(apiTries+=1);
                                             }
          });
-      }
-}
+  }
+} // close Quote componenet
 
 export default Quote;
